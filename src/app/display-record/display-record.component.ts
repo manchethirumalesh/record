835589f4c828b6cb2dbc88fc8e0a621b46d19d19
Thirumalesh <manchethirumalesh@gmail.com>
2021-03-05 22:47:08 +0530

    commit

--- src/app/display-record/display-record.component.ts
+++ src/app/display-record/display-record.component.ts
@@ -9,8 +9,11 @@ import { SharedService } from 'src/app/shared.service';
 export class DisplayRecordComponent implements OnInit {
 
   studentList:any=[]
-  studentrollnoFilter:string = ""
+  studentrollnoFilter:Number | undefined
   studentListwithoutFilter:any = []
+
+  student_Name:string=""
+  Roll: any;
 
   constructor( private service : SharedService ) { }
 
@@ -19,22 +22,74 @@ export class DisplayRecordComponent implements OnInit {
   } 
 
   refreshstudent(){
-    let rank
     this.service.getstudentlist().subscribe((data)=>{
       this.studentList=data;
       this.studentListwithoutFilter=data
     })
   }
 
-  filterfn(){
-    // var studentrollnoFilter = this.studentrollnoFilter
-    // this.studentList= this.studentListwithoutFilter.filter(function (el:any){
-    //   return el.studentList.roll_no.toString().toLowerCase().include(
-    //     studentrollnoFilter.toString().trim().toLowerCase()
-    //   )
-    // })
-    console.log(this.studentListwithoutFilter)
+   search(){
+//     // var studentrollnoFilter = this.studentrollnoFilter
+//     // this.studentList= this.studentListwithoutFilter.filter(function (el:any){
+//     //   return el.roll_no.toString().toLowerCase().include(
+//     //     studentrollnoFilter.toString().trim().toLowerCase()
+//     //   )
+//     // })
+
+    if(this.student_Name!==""){
+          this.studentList = this.studentListwithoutFilter.filter((res: { student_name: string; }) =>{
+                return res.student_name.toLowerCase().match(this.student_Name.toLowerCase())
+              })
+
+              this.Roll=""
+    }else if(this.student_Name == ""){
+      this.ngOnInit()
+    }
+
+// //     if(this.Roll!== undefined){
+// //       this.studentList = this.studentListwithoutFilter.filter((res: { roll_no: number; }) =>{
+// //             return res.roll_no.toString(this.Roll)
+// //           })
+// // }else if(this.student_Name == undefined){
+// //   this.ngOnInit()
+// // }
+//     //  if(this.student_Name!==null){
+//     //       this.studentList = this.studentListwithoutFilter.filter((res: { roll_no: number; }) =>{
+//     //             return this.student_Name
+//     //           })
+//     // }else if(this.student_Name == null){
+//     //   this.ngOnInit()
+//     // }
+ console.log(this.studentList)
+}
+
+  ser(){
+
+    if(this.Roll!== null){
+       this.studentList = this.studentListwithoutFilter.filter((res: { roll_no: number; }) =>
+        res.roll_no == this.Roll);
+        this.student_Name =""
+    }else if(this.Roll == null){
+      this.ngOnInit()
+
+    }
+
+   
+   console.log(this.studentList)   
+
   }
+
+
+  // search1(){
+  //   if(this.Roll!== undefined){
+  //            this.studentList = this.studentListwithoutFilter.filter((res: { roll_no: any; }) =>{
+  //                  return res.roll_no.
+  //            })
+  //      }else if(this.student_Name == undefined){
+  //        this.ngOnInit()
+  //      }
+  // }
+
 
   sorts(prop: any,asc: any){
     this.studentList = this.studentListwithoutFilter.sort(function(a: { [x: string]: number; },b: { [x: string]: number; }){
